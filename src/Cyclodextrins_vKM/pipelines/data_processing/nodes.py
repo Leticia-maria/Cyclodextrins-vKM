296ff2de18c7511d260766926764d14eef7d7f8f
MAIHfly <mhosfeld@andrew.cmu.edu>
2022-02-10 17:40:17 -0500

    Create nodes.py in pipelines/data_processing and populate it with necessary functions

---
+++ src/Cyclodextrins_vKM/pipelines/data_processing/nodes.py
@@ -0,0 +1,40 @@
+import autode as ade
+orca = ade.methods.ORCA()
+import numpy as np
+
+def ConvertMol(x):
+    Solvent = input('If you got a solvent give us the name here if not just hit enter: ')
+    if Solvent == '':
+        Solvent = None
+    MoI = ade.Molecule(x, solvent_name=Solvent)
+    return(MoI)
+
+def OptimizeMol(MoI):
+    MoI.optimise(method=ade.methods.XTB())
+    return(MoI)
+
+def CalculateMol(MoI):
+    NoC = input('How many cores do you have/want to use if you only have 1 hit enter: ')
+    if NoC == '':
+        NoC = 1
+    CoI = ade.Calculation(name=MoI.name,molecule=MoI,method=orca,keywords=orca.keywords.hess,n_cores=NoC)
+    CoI.output.filename = MoI.name+'.out'
+    return(CoI)
+
+def GetGibbsMol(MoI,CoI):
+    x.calc_thermo(calc=CoI)
+    print(f'G = {MoI.free_energy:.6f} Ha')
+    GibbsE = MoI.free_energy
+    return(GibbsE)
+
+def GetPka(GibbsE):
+    GibbsEkJpM = 2600*GibbsE
+    R = 0.008314
+    T = input("What's the Temperature(if you give nothing it will be 273.15K): ")
+    if T == '':
+        T = 273.15
+    LnK = GibbsE/(-1*R*T)
+    K = np.exp(LnK)
+    pKa = -1*np.log10(K)
+    print(f'pKa = {pKa}')
+    return(pKa)
