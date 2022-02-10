a515b5c276219ec4543893e1f17e17895e3d08a4
MAIHfly <mhosfeld@andrew.cmu.edu>
2022-02-10 18:12:26 -0500

    Bug Fix: fix names in pipeline.py

--- src/Cyclodextrins_vKM/pipelines/data_processing/pipeline.py
+++ src/Cyclodextrins_vKM/pipelines/data_processing/pipeline.py
@@ -5,10 +5,10 @@ from .nodes import ConvertMol, OptimizeMol, CalculateMol, GetGibbsMol, GetPka
 def create_pipeline(**kwargs):
     return Pipeline(
         [
-            node(func=ConvertMol, inputs='Butane_xyz', outputs='Butane_Molecule', name='Create Molecule'),
-            node(func=OptimizeMol, inputs='Butane_Molecule', outputs='Butane_Molecule', name='Optmize Molecule'),
-            node(func=CalculateMol, inputs='Butane_Molecule', outputs='Butane_Calculation',name='Single Point Calc'),
-            node(func=GetGibbsMol, inputs=['Butane_Molecule','Butane_Calculation'], outputs='GibbsE', name='Get Gibbs'),
-            node(func=GetPka, inputs='GibbsE',outputs='pKa',name='Get pKa')
+            node(func=ConvertMol, inputs='Butane_xyz', outputs='Butane_Molecule', name='Create_Molecule'),
+            node(func=OptimizeMol, inputs='Butane_Molecule', outputs='Butane_Molecule', name='Optmize_Molecule'),
+            node(func=CalculateMol, inputs='Butane_Molecule', outputs='Butane_Calculation',name='Single_Point_Calc'),
+            node(func=GetGibbsMol, inputs=['Butane_Molecule','Butane_Calculation'], outputs='GibbsE', name='Get_Gibbs'),
+            node(func=GetPka, inputs='GibbsE',outputs='pKa',name='Get_pKa')
         ]
     )
