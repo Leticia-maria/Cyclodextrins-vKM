e24136ee25fcccef2771ff8f7cbc1ba9e1add73a
MAIHfly <mhosfeld@andrew.cmu.edu>
2022-02-19 12:05:20 -0500

    change of inputs bl45Me2Cy

--- src/Cyclodextrins_vKM/pipelines/data_processing/pipeline.py
+++ src/Cyclodextrins_vKM/pipelines/data_processing/pipeline.py
@@ -5,11 +5,11 @@ from .nodes import ConvertMol, OptimizeMol, CalculateMol, GetGibbsMol, GetDeltGi
 def create_pipeline(**kwargs):
     return Pipeline(
         [
-            node(func=ConvertMol, inputs='bBiCyDNH_xyz', outputs='Molecule1', name='Create_Molecule1'),
+            node(func=ConvertMol, inputs='bl45Me2CyDNH_xyz', outputs='Molecule1', name='Create_Molecule1'),
             node(func=OptimizeMol, inputs='Molecule1', outputs='Molecule_opt1', name='Optmize_Molecule1'),
             node(func=CalculateMol, inputs='Molecule_opt1', outputs='Calculation1',name='Single_Point_Calc1'),
             node(func=GetGibbsMol, inputs=['Molecule_opt1','Calculation1'], outputs='GibbsE1', name='Get_Gibbs1'),
-            node(func=ConvertMol, inputs='bBiCyDNH2_xyz', outputs='Molecule2', name='Create_Molecule2'),
+            node(func=ConvertMol, inputs='bl45Me2CyDNH2_xyz', outputs='Molecule2', name='Create_Molecule2'),
             node(func=OptimizeMol, inputs='Molecule2', outputs='Molecule_opt2', name='Optimize_Molecule2'),
             node(func=CalculateMol, inputs='Molecule_opt2', outputs='Calculation2', name='Single_Point_Calc2'),
             node(func=GetGibbsMol, inputs=['Molecule_opt2','Calculation2'], outputs='GibbsE2', name='Get_Gibbs2'),
